e762eb82e3f74e385d6a922469e73e565f93ed29
logiqkys <logiqkys@gmail.com>
2025-01-22 06:49:15 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import ViewerPage from "./views/ViewerPage";
 import EditorPage from "./views/EditorPage";
 
 const App: React.FC = () => {
   return (
-    <Router basename="/PTC-React-Login">
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/viewer" element={<ViewerPage />} />
